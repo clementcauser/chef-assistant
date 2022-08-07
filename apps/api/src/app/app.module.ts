41864c467b5a31260f9838d81e0115e60b73826d
Clément Causer <34310857+clementcauser@users.noreply.github.com>
2022-08-07 14:56:36 +0200

    :tada: Backend initialized (#3)
    Nestjs app initialized
    MongoDB connected and working
    
    #3

---
+++ apps/api/src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { DatabaseModule } from "@chef-assistant/core-database";
+import { Module } from "@nestjs/common";
+
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
